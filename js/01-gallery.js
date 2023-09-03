2ae4a133b24a6b12de3c3675060f32a4ea31b0ff
Fakiru <128322346+Fakiru@users.noreply.github.com>
2023-09-03 16:51:35 +0300

    update 3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,18 +1,67 @@
-import { galleryItems } from "./gallery-items.js";
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-document.addEventListener("DOMContentLoaded", function () {
-  const gallery = document.querySelector(".gallery");
+const galleryElem = document.querySelector("ul.gallery");
 
-  gallery.addEventListener("click", function (e) {
-    e.preventDefault();
+galleryItems.forEach((item) => {
+    const newLi = document.createElement("li");
+    newLi.classList.add("gallery__item");
 
-    if (e.target.tagName === "IMG") {
-      const source = e.target.dataset.source;
-      const image = `<img src="${source}" alt="Large Image"/>`;
+    const newLiLink = document.createElement("a");
+    newLiLink.classList.add("gallery__link");
+    newLiLink.href = item.original; 
+    // newLiLink.addEventListener("click", (event) => {
+    //     event.preventDefault();
+    //     const instance = basicLightbox.create(`
+    //     <img id="modal-img" src="${item.original}">
+    //     `);
+    //     instance.show(() => { 
+    //         const callback = (event) => {
+    //             const key = event.key; 
+    //             console.log(key);
+    //             if (key === "Escape") {
+    //                 instance.close();
+    //                 document.removeEventListener("keydown", callback) 
+    //             }
+    //         };                   
+    //         document.addEventListener("keydown", callback);
+    //     });
+    // });
 
-      const modal = basicLightbox.create(image);
-      modal.show();
-    }
-  });
+    const img = document.createElement("img");
+    img.classList.add("gallery__image");
+    img.src = item.preview;
+    img.alt = item.description;
+    img.setAttribute("data-source",item.original);
+
+    newLiLink.append(img);
+    newLi.append(newLiLink);
+    galleryElem.append(newLi);
 });
+
+const aElems = document.querySelectorAll("ul a");
+aElems.forEach((elem) => {
+    elem.addEventListener("click", (event) => {
+        event.preventDefault();
+        const instance = basicLightbox.create(`
+        <img id="modal-img" src="${event.currentTarget.href}">
+        `);
+        instance.show(() => { 
+            const callback = (event) => {
+                const key = event.key;
+                
+                if (key === "Escape") {
+                    instance.close();
+                    document.removeEventListener("keydown", callback) 
+                }
+
+            };                   
+            document.addEventListener("keydown", callback);
+        });
+    });
+})
+
+
+
+
+
